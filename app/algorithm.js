6cd06b6acb7094cda9cf14f5132695517660b690
Nicholas Westbury <5westbury5@gmail.com>
2017-01-28 14:11:30 -0500

    added algorithm skelethon

---
+++ app/algorithm.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+This function exposes one function, 'compute_room'
+that will be called from the interface to find
+the optimal room layout.
+ */
+
+var compute_room;
+
+(function(){
+    function eval_room(){
+
+
+    }
+
+    compute_room = function(room_objects){
+
+
+    }
+
+})();
