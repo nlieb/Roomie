11a4fecaec20e0ba6f0d12f01b46c5d96c27d6d5
eyesniper2 <eyesniper2@rayfall.net>
2017-01-29 00:54:51 -0800

    Cleaned up uicontroller

--- app/uicontroller.js
+++ app/uicontroller.js
@@ -21,6 +21,43 @@ export default class UIController {
             $btn.closest('tr').fadeOut();
         });
         $('#btnAddItem').on('click', () => this.addObject(this.app.state));
+
+        let noUiSlider = require('nouislider'); 
+
+        let sliderWidth = document.getElementById('sliderWidth');
+        let sliderHeight = document.getElementById('sliderHeight');
+
+        noUiSlider.create(sliderWidth, {
+        start: 100,
+        connect: 'lower',
+        step: 1,
+        range: {
+            min: 50,
+            max: 150
+        },
+        });
+
+        let sliderWidthDisplayValue = document.getElementById('sliderWidthDisplayValue');
+
+        sliderWidth.noUiSlider.on('update', function( values, handle ) {
+            sliderWidthDisplayValue.innerHTML = 'Room Width: ' + parseInt(values[handle]);
+        });
+
+        noUiSlider.create(sliderHeight, {
+        start: 100,
+        step: 1,
+        connect: 'lower',
+        range: {
+            min: 50,
+            max: 150
+        }
+        });
+
+        let sliderHeightDisplayValue = document.getElementById('sliderHeightDisplayValue');
+
+        sliderHeight.noUiSlider.on('update', function( values, handle ) {
+            sliderHeightDisplayValue.innerHTML = 'Room Length: ' + parseInt(values[handle]);
+        });
     }
 
     addObject(state) {
